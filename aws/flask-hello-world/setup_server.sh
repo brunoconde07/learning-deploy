9c80356eea37aff0617ca8f3882fd4ac3969593f
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-12-03 02:39:06 -0300

    [aws] -> only used steps (now, it's until deploy) -> (run_server.sh, setup_server.sh, test_server.sh, and steps.txt, at aws/flask-hello-world)

---
+++ aws/flask-hello-world/setup_server.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt-get install python3.8
+sudo apt install python3.8-distutils
+
+# https://pip.pypa.io/en/stable/installation/
+# https://stackoverflow.com/questions/61717006/pip-for-python-3-8
+
+# download
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python3.8 get-pip.py
+# upgrade
+python3.8 -m pip install --upgrade pip
---
+++ aws/flask-hello-world/test_server.sh
@@ -0,0 +1,2 @@
+# On amazon platform, back to Connect to instance > SSH Client > 4. Connect to your instance using its Public DNS
+curl ec2-3-15-145-47.us-east-2.compute.amazonaws.com:8080
